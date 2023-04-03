41eebe3c7f3242b67a713c8baa38ff1b2d098554
Ashish <anshm9448@gmail.com>
2023-04-03 18:58:25 +0530

    Final commit

--- src/main/java/com/pythonbot/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/pythonbot/employeemanagement/controller/EmployeeController.java
@@ -1,17 +1,69 @@
 package com.pythonbot.employeemanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pythonbot.employeemanagement.exceptions.ResourceNotFoundException;
+import com.pythonbot.employeemanagement.model.Employee;
+import com.pythonbot.employeemanagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("api/vi/")
+@RequestMapping("/api/v1")
 public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
     //get employee
 
+    @GetMapping("/getemployees")
+    public List<Employee> getAllEmployee(){
+
+        return this.employeeRepository.findAll();
+    }
     //get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        return ResponseEntity.ok().body(employee);
+    }
 
     //save employee
 
+    @PostMapping("/employees")
+    public Employee createEmployee( @RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+                                                    @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setLastname(employeeDetails.getLastname());
+        employee.setFirstname(employeeDetails.getFirstname());
+        final Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
     //update employee
 
     //delete employee
--- src/main/java/com/pythonbot/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/pythonbot/employeemanagement/repository/EmployeeRepository.java
@@ -1,10 +1,11 @@
 package com.pythonbot.employeemanagement.repository;
 
+import com.pythonbot.employeemanagement.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long > {
 
 
 }
